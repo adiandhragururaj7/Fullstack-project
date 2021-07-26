6886e6eb07c6bcd2b4207bd1e04ab57fe09117de
adiandhragururaj7 <adiandhragururaj7@gmail.com>
2021-07-26 05:46:23 +0530

    added bootstrap cdn

---
+++ script.js
@@ -0,0 +1,13 @@
+const info = ['name', 'address' , 'mobile' , 'email',[241,242,243]];
+let key = info[1];
+console.log(key);
+console.log(info.indexOf('mobile'));
+info.push('companyid');
+info.shift();
+console.log(info);
+console.log(info[3][0]);
+const people = {
+    firstname = 'johnl',
+    lastname= 'smith',
+    age =  55;
+}
